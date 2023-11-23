97439f0abf1d0c3a489328ac2e93dc8335f33121
NerokArtworks <nerokprodscontact@gmail.com>
2023-11-23 17:26:55 +0100

    update | Before sockets

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,55 @@
-const ChannelIdPage = () => {
-    return ( 
-        <div>
-            Channel Id Page
-        </div>
-     );
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
- 
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  // Obtener el channel
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  // Obtener el miembro mediante el serverId y el profileId
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile?.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  // Comprobar que exista el perfil (este logueado)
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  // Vista del channel / conversation
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      {/* Header */}
+      <ChatHeader
+        serverId={channel.serverId}
+        name={channel.name}
+        type={"channel"}
+      />
+    </div>
+  );
+};
+
 export default ChannelIdPage;
--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -19,10 +19,24 @@ export async function PATCH(
             return new NextResponse("Server ID Missing", { status: 400 });
         }
 
+        // Fix si no soy admin mi profile.id no se corresponde con el server.profileId (no soy el creador) Entonces si quiero hacer el update filtrando por serverId y profileId necesito obtener el server.profileId a traves del serverId que ya conozco
+        const serverProfile = await db.server.findUnique({
+            where: {
+                id: params.serverId,
+            },
+            select: {
+                profileId: true,
+            },
+        });
+
+        if (!serverProfile) {
+            return new NextResponse("Server not found", { status: 404 });
+        }
+
         const server = await db.server.update({
             where: {
                 id: params.serverId,
-                profileId: profile.id,
+                profileId: serverProfile.profileId,
             },
             data: {
                 inviteCode: uuidbv4(),
@@ -31,7 +45,9 @@ export async function PATCH(
 
         return NextResponse.json(server);
     } catch (error) {
-        console.log("[SERVER_ID]", error);
+        console.error("[SERVER_ID] Error:", error);
+        console.error("[SERVER_ID] Params:", params.serverId);
         return new NextResponse("Internal Error", { status: 500 });
     }
+
 }
